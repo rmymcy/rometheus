bc0423acf3b537784d2690c93a5bdcec2c59b7f6
naysa moreno <rmymcy@gmail.com>
2020-06-14 20:47:29 -0500

    Updates

---
+++ precache-manifest.bda492e1bdef766d156ab7126349f368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0891dfa37719eb16836777370a7bf2f",
+    "url": "/rometheus/index.html"
+  },
+  {
+    "revision": "534cdd210cab014a6ce2",
+    "url": "/rometheus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfc59dd673fc1defaaad",
+    "url": "/rometheus/static/js/2.4593c607.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rometheus/static/js/2.4593c607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534cdd210cab014a6ce2",
+    "url": "/rometheus/static/js/main.196de91c.chunk.js"
+  },
+  {
+    "revision": "b88122d54fd598568bb9",
+    "url": "/rometheus/static/js/runtime-main.3865d8b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rometheus/static/media/logo.5d5d9eef.svg"
+  }
+]);
