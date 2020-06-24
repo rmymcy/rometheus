3e9fcd175b922141e4474bc3e0f57fe449005488
rmymcy <rmymcy@gmail.com>
2020-06-23 20:48:43 -0500

    set up navigation bar

--- rometheus/src/index.js
+++ rometheus/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
+import Navbar, {NavButton} from './navigation/navbar'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class App {
+  root=document.getElementById("root")
+  show(component){
+    ReactDOM.render(component,this.root)
+  }
+}
+
+const app=new App()
+
+app.show(
+<Navbar>
+  <NavButton>start</NavButton>
+  <NavButton>end</NavButton>
+</Navbar>
 );
 
 // If you want your app to work offline and load faster, you can change
